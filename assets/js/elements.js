7ff88effe4bee545d76060578b0e10ef82345b81
Linus Norström <fh5cm@outlook.com>
2023-01-12 19:18:44 +0100

    split js into constants and script and made dev branch

---
+++ assets/js/elements.js
@@ -0,0 +1,46 @@
+// -----------------------------------------------------------------------
+// HTML elements
+// -----------------------------------------------------------------------
+
+let eStart = document.getElementById("start");
+let eStartName = document.getElementById("startName");
+let eStartCarName = document.getElementById("startCarName");
+let eStartCarMake = document.getElementById("startCarMake");
+let eStartCarModel = document.getElementById("startCarModel");
+let eStartLoad = document.getElementById("startLoad");
+
+let eGame = document.getElementById("game");
+let eStateName = document.getElementById("stateName");
+let eStateCar = document.getElementById("stateCar");
+let eStateCredits = document.getElementById("stateCredits");
+let eStateDR = document.getElementById("stateDR");
+let eStateDRProgress = document.getElementById("stateDRProgress");
+let eEvents = document.getElementById("eventsDiv");
+let eEventsT = document.getElementById("eventsTable");
+let eEventsTH = document.getElementById("eventsTableHead");
+let eEventsTB = document.getElementById("eventsTableBody");
+let eRacesT = document.getElementById("racesTable");
+let eRacesTH = document.getElementById("racesTableHead");
+let eRacesTB = document.getElementById("racesTableBody");
+let eGarageTH = document.getElementById("garageTableHead");
+let eToggleOptions = document.getElementById("toggleOptionsButton");
+let eGarageTB = document.getElementById("garageTableBody");
+let eNewCarRow = document.getElementById("newCarRow");
+let eNewCarName = document.getElementById("newCarName");
+let eNewCarMake = document.getElementById("newCarMake");
+let eNewCarModel = document.getElementById("newCarModel");
+let eGameSpeed = document.getElementById("gameSpeed");
+let eRoadSprints = document.getElementById("roadSprintsCheck");
+let eRoadCircuits = document.getElementById("roadCircuitsCheck");
+let eDirtTrails = document.getElementById("dirtTrailsCheck");
+let eDirtScrambles = document.getElementById("dirtScramblesCheck");
+let eCrossCountrySprints = document.getElementById("crossCountrySprintsCheck");
+let eCrossCountryCircuits = document.getElementById("crossCountryCircuitsCheck");
+let eStreetSprints = document.getElementById("streetSprintsCheck");
+let eAutoshow = document.getElementById("autoshowCheck");
+let eCarPass = document.getElementById("carPassCheck");
+let eHotWheels = document.getElementById("hotWheelsCheck");
+let eWelcome = document.getElementById("welcomeCheck");
+let eBarnFind = document.getElementById("barnFindCheck");
+let eGameLoad = document.getElementById("gameLoad");
+
